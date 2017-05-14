0874dff1c6fdf0559f32dc40ae7af821ebf63143
j-west <westja89@gmail.com>
2017-05-13 20:56:41 -0500

    Add logic to display a users monthly budget information to the page

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import { LineChart, PieChart, AreaChart } from 'react-chartkick';
-
 import { getUserBudgets } from '../actions'
 
 class Chart extends Component {
@@ -11,11 +10,12 @@ class Chart extends Component {
   }
 
   displayBudgets() {
-    console.log(this.props)
+
     return _.map(this.props.budgets, budget => {
       return (
       <li key={budget._id}>
-        {budget.month}
+        <p>{budget.month}</p>
+        {budget.expenses.map(expense => <p>{expense}</p>)}
       </li>
       )
     })
@@ -23,22 +23,23 @@ class Chart extends Component {
 
 
   render() {
+ let data = [
+   {"name":"Workout", "data": {"2013-02-10 00:00:00 -0800": 3, "2013-02-17 00:00:00 -0800": 4}},
+   {"name":"Call parents", "data": {"2013-02-10 00:00:00 -0800": 5, "2013-02-17 00:00:00 -0800": 3}}
+ ];
 
-//       let data = [
-//   {"name":"Workout", "data": {"2013-02-10 00:00:00 -0800": 3, "2013-02-17 00:00:00 -0800": 4}},
-//   {"name":"Call parents", "data": {"2013-02-10 00:00:00 -0800": 5, "2013-02-17 00:00:00 -0800": 3}}
-// ];
-
+    if (!this.props.budgets) {
+      return <div>Loading budgets now. </div>
+    }
 
         return  (
           <div>
             <ul>
               {this.displayBudgets()}
             </ul>
-            {/*<AreaChart data={data} />*/}
+            <AreaChart data={data} />
           </div>
       )}
-
  }
 
  function mapStateToProps(state) {
