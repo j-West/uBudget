2e18d08d2ff97c273a16604251314d7bfe5981c4
j-west <westja89@gmail.com>
2017-05-17 13:57:00 -0500

    Add redux-thunk.  Add component imports and add a Route for each component

--- src/index.js
+++ src/index.js
@@ -4,25 +4,36 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import promise from 'redux-promise'
+import reduxThunk from 'redux-thunk'
 
 import Home from './components/Home'
 import Features from './components/Features'
 import Register from './components/Register'
 import Profile from './components/Profile'
-import reducers from './reducers';
+import SignUp from './components/SignUp'
+import SignIn from './components/SignIn'
+import SignOut from './components/SignOut'
+import RequireAuth from './components/RequireAuth'
+import rootReducer from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk, promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(rootReducer)}>
     <BrowserRouter>
       <div>
         <Switch>
           <Route path='/register' component={ Register } />
           <Route path='/features' component={ Features } />
-          <Route path='/profile' component={ Profile } />
-          <Route path='/' component={ Home } />
+          <Route path='/profile' component={ RequireAuth(Profile) } />
+          <Route path='/signup' component={ SignUp } />
+          <Route path='/signin' component={ SignIn } />
+          <Route path='/signout' component={ SignOut } />
+          <Route path='/' component={ Home }/>
         </Switch>
       </div>
-    </BrowserRouter>
+  </BrowserRouter>
   </Provider>, document.querySelector('.container-fluid'));
