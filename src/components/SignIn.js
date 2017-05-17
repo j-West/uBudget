222c3702c519b64a70a9cb3314b01d8a0ef99aa0
j-west <westja89@gmail.com>
2017-05-17 14:00:35 -0500

    Move LoginForm code to SignIn component.  Using redux-form to capture and validate user input and send it to signInUp

---
+++ src/components/SignIn.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router'
+import { signInUp } from '../actions'
+
+import Navbar from './Navbar'
+
+class SignIn extends Component {
+
+
+  renderField(field) {
+
+
+    const { meta: { touched, error }} = field
+
+    const divClassNames = `form-group ${ touched && error ? 'text-danger' : ''}`
+    return (
+      <div className={ divClassNames }>
+        <label>{ field.label }</label>
+        <input
+          type={ field.label === 'Password' ? 'password' : 'text' }
+          className="form-control col-7"
+          { ...field.input }
+        />
+        <div className='text-danger'>
+          { touched ? error : ' '}
+        </div>
+      </div>
+    )
+  }
+
+  onSubmit(values) {
+    this.props.signInUp(values, 'signin')
+}
+
+renderAlert() {
+  if(this.props.errorMessage) {
+    return (
+      <div className='alert alert-danger'>
+        <strong>
+          {this.props.errorMessage}
+        </strong>
+      </div>
+    )
+  }
+  else if(this.props.authenticated) {
+    return <Redirect to='/profile' />
+  }
+}
+
+
+  render() {
+
+    const { handleSubmit } = this.props
+
+    return (
+      <div>
+        <Navbar />
+          <form className='col-lg-4' onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+            <Field
+              label="Email"
+              name='email'
+              component={ this.renderField }
+            />
+            <Field
+              label='Password'
+              name='password'
+              component={ this.renderField }
+            />
+            {this.renderAlert()}
+            <button type='submit' className='btn'>Submit</button>
+          </form>
+    </div>
+    )
+  }
+}
+
+
+function validate(values) {
+  const HTML5_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  const errors = {}
+
+  if (!HTML5_EMAIL_REGEX.test(values.email)) {
+    errors.email = "Please enter a valid email"
+  }
+
+  if (!values.password) {
+    errors.password = "Please enter your password"
+  }
+
+  return errors
+}
+
+const mapStateToProps = (state) => {
+  return {
+    errorMessage: state.auth.error,
+    authenticated: state.auth.authenticated
+  }
+}
+
+
+export default reduxForm({
+  validate,
+  form: 'UserLoginForm'
+})(
+  connect(mapStateToProps, { signInUp })(SignIn))
