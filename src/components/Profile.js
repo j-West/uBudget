58c559dd125c784ce2bfba0c0a3523f223398649
j-west <westja89@gmail.com>
2017-05-18 11:02:07 -0500

    Connected profile to state to pass props to children components

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react'
 import { Field,  reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 import { addExpense } from '../actions'
+import { getUserBudgets } from '../actions'
 
 import Navbar from './Navbar'
 import Chart from './Chart'
 import NewExpense from './NewExpense'
 
 
-export default class Profile extends Component {
+class Profile extends Component {
+
   render() {
     return (
       <div>
         <Navbar />
-        <Chart />
-        <NewExpense type='Expense'/>
+        <Chart budgets={this.props.budgets} />
+        <NewExpense type='Expense' addExpense={this.props.addExpense()} />
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+ return {
+   budgets: state.userBudgets.budgets,
+   userId: state.auth.userId
+ }
+}
+
+export default connect(mapStateToProps, { getUserBudgets, addExpense })(Profile)
