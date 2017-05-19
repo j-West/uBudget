b0cc586fc3cfe95ca4a15f97b49a0a9edda1a201
j-west <westja89@gmail.com>
2017-05-19 05:58:00 -0500

    Add three sample charts to the HomeDetails component

--- src/components/HomeDetails.js
+++ src/components/HomeDetails.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { PieChart } from 'react-chartkick'
 
 export default class HomeDeatils extends Component {
   render() {
     return (
-        <div className="col-lg-7 col-md-auto">
-          <img className="card-img-top img" src="../../public/images/cashRoll.jpg" alt="Card image cap" />
+        <div className="flex-container-budget">
+          <PieChart data={[["Gas", 3], ["Bill", 8], ["Food", 5]]} />
+          <PieChart data={[["Gas", 5], ["Bill", 5], ["Food", 6]]} />
+          <PieChart data={[["Gas", 7], ["Bill", 4], ["Food", 5]]} />
         </div>
     )
   }
