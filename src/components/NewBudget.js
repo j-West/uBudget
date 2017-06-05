6738385bc771ff6f9877ba2530e1de137266a7c8
j-west <westja89@gmail.com>
2017-06-05 13:20:19 -0500

    Change layout of newBudget component and add instructions below form

--- src/components/NewBudget.js
+++ src/components/NewBudget.js
@@ -17,7 +17,7 @@ class NewBudget extends Component {
         <label>{ field.label }</label>
         <input
           type="text"
-          className="form-control col-7"
+          className="form-control"
           { ...field.input }
         />
         <div className='text-danger'>
@@ -39,9 +39,9 @@ class NewBudget extends Component {
     const { handleSubmit } = this.props
 
     return (
-      <div className='new-expense-bg'>
-
-        <form className='flex-container-budget' onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+      <div className='col-lg-5 offset-lg-1'>
+        <h2 className='text-center'>Add New Budget</h2>
+        <form className='' onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
           <Field
             label='Budget'
             name='budgetName'
@@ -49,6 +49,10 @@ class NewBudget extends Component {
           />
           <button type='submit' className='btn small-btn btn-success'>Create Budget</button>
         </form>
+        <div className='mt-3'>
+          <p>To get started, create a new budget by entering a name for the budget and clicking the "Create Budget" button.</p>
+          <p>After creating a budget, you can start adding expenses to it by entering the amount you spent, the budget you wish to add the expense to, and the category of the expense.</p>
+        </div>
     </div>
     )
   }
