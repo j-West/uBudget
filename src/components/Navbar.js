43c49868d03304c0b6bd2014dd66ca51cd05f3f3
j-west <westja89@gmail.com>
2017-06-05 13:19:36 -0500

    Remove bottom margin frmo Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ class Navbar extends Component {
     }
   render() {
     return (
-      <div className='bottom-margin-5 no-gutter'>
+      <div className='no-gutter'>
         <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle   navigation">
             <span className="navbar-toggler-icon"></span>
