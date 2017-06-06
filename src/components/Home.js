9741b5c1a146af84b708d1f93c62fc9c11167f8e
j-west <westja89@gmail.com>
2017-06-06 16:52:49 -0500

    Change names of budget entries on Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,8 @@ export default class Home extends Component {
           <h1 className='text-center display-1 mb-3 top-margin-5'>uBudget</h1>
           <h4 className='text-center bottom-margin-5'>Register today and start seeing how much you spend on what!</h4>
           <div className="flex-container-budget mt-3">
-            <PieChart className='' data={[["Gas", 3], ["Bill", 8], ["Food", 5]]} />
-            <PieChart className='' data={[["Gas", 5], ["Kids", 5], ["Grocery", 6], ["Pet", 3]]} />
+            <PieChart className='' data={[["Car", 3], ["Bills", 8], ["Food", 5]]} />
+            <PieChart className='' data={[["Gas", 5], ["Kids", 5], ["Rent", 6], ["Pet", 3]]} />
             <PieChart className='' data={[["Gas", 7], ["Bill", 4], ["Food", 5]]} />
           </div>
       </div>
