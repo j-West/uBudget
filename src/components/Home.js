7f2731c9cb49a1cd47712b967be3a69710656f97
j-west <westja89@gmail.com>
2017-06-07 17:04:13 -0500

    Add div to hold PieChart for small screens. change classNames

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,10 +14,13 @@ export default class Home extends Component {
         <Navbar />
           <h1 className='text-center display-1 mb-3 top-margin-5'>uBudget</h1>
           <h4 className='text-center bottom-margin-5'>Register today and start seeing how much you spend on what!</h4>
-          <div className="flex-container-budget mt-3">
-            <PieChart className='' data={[["Car", 3], ["Bills", 8], ["Food", 5]]} />
-            <PieChart className='' data={[["Gas", 5], ["Kids", 5], ["Rent", 6], ["Pet", 3]]} />
-            <PieChart className='' data={[["Gas", 7], ["Bill", 4], ["Food", 5]]} />
+          <div className="mt-3 show-large">
+            <PieChart data={[["Car", 3], ["Bills", 8], ["Food", 5]]} />
+            <PieChart data={[["Gas", 5], ["Kids", 5], ["Rent", 6], ["Pet", 3]]} />
+            <PieChart data={[["Gas", 7], ["Bill", 4], ["Food", 5]]} />
+          </div>
+          <div className="mt-3 show-small">
+            <PieChart data={[["Gas", 5], ["Kids", 5], ["Rent", 6], ["Pet", 3]]} />
           </div>
       </div>
     )
